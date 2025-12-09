ad9fa8982eef1b29b48bab60a30cf9dab6f121ee
Rony <rony.rangel@upe.br>
2025-12-09 15:50:32 -0300

    Adicionando os arquivos referentes ao Strapi

---
+++ strapi/config/plugins.ts
@@ -0,0 +1,5 @@
+export default () => ({
+    geodata: {
+    enabled: true,
+  },
+});
---
+++ strapi/src/api/empreendimento/controllers/empreendimento.ts
@@ -0,0 +1,7 @@
+/**
+ * empreendimento controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::empreendimento.empreendimento');
